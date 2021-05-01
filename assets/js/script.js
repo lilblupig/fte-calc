c783dfb73625a812338e2e39209b59de7d3a9271
lilblupig <lilblupig@gmail.com>
2021-05-01 19:51:46 +0000

    Sort event listener button types

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,20 @@ const buttons = document.querySelectorAll(".btn");
 buttons.forEach(function(button){
     button.addEventListener("click", function(event) {
         let classes = event.currentTarget.classList;
-        console.log(this.innerHTML);
+        if(classes.contains("region-btn")) {
+            console.log(this.innerHTML);
+        } else if (classes.contains("grade-btn")) {
+            console.log(this.innerHTML);
+        } else if (classes.contains("scp-btn")) {
+            console.log(this.innerHTML);
+        } else if (classes.contains("weeks-btn")) {
+            console.log(this.innerHTML);
+        } else if (classes.contains("service-btn")) {
+            console.log(this.innerHTML);
+        } else {
+            console.log("Unknown button type");
+        }
     })
 });
+
+console.log(this.innerHTML);
